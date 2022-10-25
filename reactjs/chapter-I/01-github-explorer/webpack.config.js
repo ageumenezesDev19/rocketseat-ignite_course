86a436f64335af9027a4377e2f8a6bd81dd33d5d
ageu_dev19 <ageumenezes423@gmail.com>
2022-10-24 22:41:36 -0300

    Configure the dev server dependency

--- reactjs/chapter-I/01-github-explorer/webpack.config.js
+++ reactjs/chapter-I/01-github-explorer/webpack.config.js
@@ -19,6 +19,9 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx'],
   },
+  devServer : {
+    watchFiles: path.resolve(__dirname, 'public'),
+  },
   plugins: [
     new htmlWebPackPlugin({
         template: path.resolve(__dirname, 'public', 'index.html'),
