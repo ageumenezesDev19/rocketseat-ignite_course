c32e3737ac3775908ca4b9bd3ecf120dddeb258a
ageu_dev19 <ageumenezes423@gmail.com>
2022-10-28 18:10:39 -0300

    Testing typeScript

---
+++ reactjs/chapter-I/typeScript-introduction/test.ts
@@ -0,0 +1,26 @@
+type User = {
+    name: string,
+    email: string,
+    address: {
+        city: string,
+        state?: string,
+    }
+}
+
+function showWelcomeMessage(user: User) {
+    return `Welcome ${user.name},
+    you email is ${user.email}.
+    Your city is ${user.address.city}
+    and your state is ${user.address.state}.`;
+}
+
+const message = showWelcomeMessage({
+    name: 'John Doe',
+    email: 'john@doe.com',
+    address: {
+        city: 'New York',
+        state: 'NY',
+    }
+});
+
+console.log(message);
