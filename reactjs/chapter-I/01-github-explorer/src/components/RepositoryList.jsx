695bc47acbe4d865848a872447215baa06479b6d
ageu_dev19 <ageumenezes423@gmail.com>
2022-10-28 17:35:52 -0300

    Ends the API call and renders the Application components within the conforms.

--- reactjs/chapter-I/01-github-explorer/src/components/RepositoryList.jsx
+++ reactjs/chapter-I/01-github-explorer/src/components/RepositoryList.jsx
@@ -2,14 +2,6 @@ import { useState, useEffect  } from 'react';
 import { RepositoryItem } from "./repositoryItem";
 import '../styles/repositories.scss';
 
-// API https://api.github.com/orgs/rocketseat/repos
-
-const repository = {
-    name: "unform",
-    description: "Forms in react",
-    link: "https://github.com/unform/unform"
-}
-
 export function RepositoryList() {
 
     const [repositories, setRepositories] = useState([]);
@@ -24,9 +16,12 @@ export function RepositoryList() {
             <h1>Lista de repositórios</h1>
 
             <ul>
-                <RepositoryItem repository={ repository } />
-                <RepositoryItem repository= { repository } />
-                <RepositoryItem repository= { repository } />
+                {repositories.map(repository => {
+                    return <RepositoryItem
+                    key={repository.id}
+                    repository={ repository }
+                    />
+                })}
             </ul>
         </section>
     );
