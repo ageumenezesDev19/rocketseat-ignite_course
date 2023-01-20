0b7ed90f33a0dc1b2568754ce5b930265121c839
ageu_dev19 <ageumenezes423@gmail.com>
2023-01-20 17:28:22 -0300

    Add Post Data

--- reactjs/updated-content/project-1/ignite-feed/src/App.jsx
+++ reactjs/updated-content/project-1/ignite-feed/src/App.jsx
@@ -4,6 +4,37 @@ import { Sidebar } from "./components/Sidebar";
 import './global.css';
 import styles from './App.module.css';
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatar: 'https://avatars.githubusercontent.com/u/89534178?v=4',
+      nome: 'Ageu Menezes',
+      role: 'Web Developer',
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: 'ageumenezesdev19.github.io'}
+    ],
+    publishedAt:  new Date('2022-05-03 20:00:00'),  
+  },
+  {
+    id: 2,
+    author: {
+      avatar: 'https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=40',
+      nome: 'Alex Martins',
+      role: 'Educator',
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: 'ageumenezesdev19.github.io'}
+    ],
+    publishedAt:  new Date('2022-05-10 20:00:00'),  
+  }
+];
+
 export function App() {
 
   return (
@@ -13,8 +44,16 @@ export function App() {
       <div className={styles.wrepper}>
         <Sidebar/>
         <main>
-          <Post />
-        </main>
+          {posts.map((post) => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          } )} 
+        </main> 
       </div>
     </div>
   )
