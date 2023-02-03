939e9b2948272ccb9bd2b702fed96878373b7afe
ageu_dev19 <ageumenezes423@gmail.com>
2023-02-03 18:03:03 -0300

    Convert Ignite Feed project to TypeScript

---
+++ reactjs/updated-content/project-1/ignite-feed-ts/src/App.tsx
@@ -0,0 +1,64 @@
+import { Post } from "./components/Post";
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+import './global.css';
+import styles from './App.module.css';
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://avatars.githubusercontent.com/u/89534178?v=4',
+      name: 'Ageu Menezes',
+      role: 'Web Developer',
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: 'ageumenezesdev19.github.io'},
+      // {type: 'link-#', content: ' #novoprojeto'},
+      // {type: 'link-#', content: ' #nlw'},
+      // {type: 'link-#', content: ' #rocketseat'}
+    ],
+    publishedAt:  new Date('2022-05-03 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=40',
+      name: 'Alex Martins',
+      role: 'Educator',
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: 'ageumenezesdev19.github.io'}
+    ],
+    publishedAt:  new Date('2022-09-10 20:00:00'),  
+  }
+];
+
+export function App() {
+
+  return (
+    <div className="App">
+      <Header />
+
+      <div className={styles.wrepper}>
+        <Sidebar/>
+        <main>
+          {posts.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          } )} 
+        </main> 
+      </div>
+    </div>
+  )
+}
---
+++ reactjs/updated-content/project-1/ignite-feed-ts/src/components/Post.tsx
@@ -0,0 +1,118 @@
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import { useState } from 'react';
+import { Avatar } from './Avatar'
+import { Comment } from './Comment'
+import styles from './Post.module.css'
+
+interface Autor {
+  name: string;
+  role: string;
+  avatarUrl: string;
+}
+
+interface PostProps {
+  author: Autor;
+  publishedAt: Date;
+  content: string;
+}
+
+export function Post({ author, publishedAt, content }:PostProps ) {
+    
+    const [comments, setComments] = useState([
+      'Post muito bacana, hein?!'
+    ]);
+    
+    const [newCommentText, setNewCommentText] = useState('');
+
+    function handleCreateNewComment(event) {
+      event.preventDefault();
+
+      setComments([...comments, newCommentText]);
+      setNewCommentText(''); 
+    }
+
+    function handleNewCommentChange({ target }) {
+      target.setCustomValidity('');
+      setNewCommentText(target.value); 
+    }
+
+    const dateFormat = format(publishedAt, "dd 'de' LLLL 'ás' HH:mm'h'", {
+      locale: ptBR,
+    });
+
+    const publishedDate = formatDistanceToNow(publishedAt, {
+      locale: ptBR,
+      addSuffix: true,
+    });
+    
+    function handleNewCommentInvalid({ target }) {
+      target.setCustomValidity('Este campo é obrigatório!');
+    }
+
+    function deleteComment(commentToDelete) {
+      const commentsWithoutDeletedOne =  comments.filter(comment => {
+         return comment !== commentToDelete;
+      })
+
+      setComments(commentsWithoutDeletedOne);
+    }
+
+    const isNewCommentEmpty = newCommentText.length === 0;
+
+    return(
+        <article className={styles.post}> 
+          <header>
+            <div className={styles.author}>
+                <Avatar src={author.avatarUrl} />
+                <div className={styles.authorInfo}>
+                    <strong>{author.name}</strong>
+                    <span>{author.role}</span>
+                </div>
+            </div>
+
+            <time title={dateFormat} dateTime={publishedAt.toISOString()}>{publishedDate}</time>
+          </header>
+ 
+          <div className={styles.content}>
+            {content.map((line) => {
+              if (line.type === 'paragraph') {
+                return <p key={line.comment}>{line.content}</p>
+              } else if (line.type === 'link') {
+                return <p key={line.comment}><a href="#">{line.content}</a></p> 
+              }
+            })}
+          </div>
+
+          <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
+            <strong>Deixe seu feedback</strong>
+
+            <textarea
+              onChange={handleNewCommentChange} 
+              name="comment"
+              placeholder='Escreva um comentário...'
+              value={newCommentText}
+              onInvalid={handleNewCommentInvalid} 
+              required
+            />
+            <footer>
+              <button type="submit" disabled={isNewCommentEmpty}>
+                Publicar
+              </button>
+            </footer>
+          </form>
+
+          <div className={styles.commentList}>
+            {comments.map((comment) => {
+               return(
+                <Comment
+                  key={comment}
+                  content={comment}
+                  onDeleteComment={deleteComment} 
+                />
+               )
+            })}
+          </div>
+        </article>
+    )
+} 
