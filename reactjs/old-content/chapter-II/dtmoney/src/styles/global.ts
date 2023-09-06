52dd91cac76ba7392afeeb33d2fd0c310a70e1ce
ageu_dev19 <ageumenezes423@gmail.com>
2023-09-06 10:44:22 -0300

    Add Dashboard and Summary

--- reactjs/old-content/chapter-II/dtmoney/src/styles/global.ts
+++ reactjs/old-content/chapter-II/dtmoney/src/styles/global.ts
@@ -2,17 +2,17 @@ import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
   :root {
-    --background: #f0f2f5;
     --red: #E52E4D;
     --blue: #5429CC;
+    --green: #33CC95;
     
     --blue-light: #6933FF;
 
     --text-title: #363F5F;
     --text-body: #969CB3;
 
-    --background: #FOF2F5;
-    --shape: #FFFFFFF;
+    --background: #F0F2F5;
+    --shape: #FFFFFF;
   }
 
   * {
